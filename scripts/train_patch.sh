106fb68b1b3cb07945ca551d48d22c480bb88b9f
hengyuwen <hengyuwen@outlook.com>
2020-12-05 17:52:57 +0000

    Update sbatch training scripts for Irris5

---
+++ scripts/train_patch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:2
+#SBATCH --partition=gpu
+#SBATCH --nodes=2
+#SBATCH --time=01:00:00
+#SBATCH --no-requeue
+#SBATCH --exclusive
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=yh1n19@soton.ac.uk
+export STUDENT_ID=$(whoami)
+
+export DATA_HOME=/scratch/$(STUDENT_ID)
+
+export DATA_PATH=$(DATA_HOME)/data/material/MINC
+
+
+source .bashrc
+conda activate cv-pytorch
+cd ..
+
+python main.py --data-dir $(DATA_PATH) --stage patch
