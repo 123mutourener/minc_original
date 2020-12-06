7f471a3660c2511a50862abbcdd573ccae163be4
hengyuwen <hengyuwen@outlook.com>
2020-12-06 22:04:18 +0000

    Update file path

--- scripts/train_patch.sh
+++ scripts/train_patch.sh
@@ -15,7 +15,7 @@ export STUDENT_ID=$(whoami)
 
 export DATA_HOME=/scratch/${STUDENT_ID}
 
-export DATA_PATH=${DATA_HOME}/data/material/MINC
+export DATA_PATH=${DATA_HOME}/data/material/minc
 
 conda init
 
