3b1fb3d17def08aa8462bac6d2d0c23889c716c1
hengyuwen <hengyuwen@outlook.com>
2020-12-16 00:28:20 +0000

    Update resume.sh

---
+++ scripts/resume_patch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:2
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --time=60:00:00
+#SBATCH --no-requeue
+#SBATCH --exclusive
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=yh1n19@soton.ac.uk
+export STUDENT_ID=$(whoami)
+
+export DATA_HOME=/scratch/${STUDENT_ID}
+
+export DATA_PATH=${DATA_HOME}/data/material
+
+conda init
+
+source /local/software/conda/miniconda-py3-new/bin/activate cv-pytorch
+
+conda activate cv-pytorch
+
+cd ..
+
+python main.py --data-root "${DATA_PATH}" --stage patch --batch-size 128 --resume ./results/pytorch_densenet121_minc_EJCtVTG6_latest.json
