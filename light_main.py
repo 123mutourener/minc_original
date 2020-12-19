fb0da5441773be81c628150686331388ee8b96b1
hengyuwen <hengyuwen@outlook.com>
2020-12-19 21:48:04 +0000

    https://pytorch-lightning.readthedocs.io/en/latest/trainer.html?highlight=test#test

--- light_main.py
+++ light_main.py
@@ -1,10 +1,12 @@
 from pytorch_lightning import Trainer
 from pytorch_lightning.utilities.seed import seed_everything
 import torch
+import os
 from pytorchtools.MINCDataModule import MINCDataModule
 from pytorchtools.arg_parser import ArgParser
 from pytorchtools.patch_classifier import PatchClassifier
-from pytorchtools.callbacks import valid_acc_callback, valid_loss_callback
+from pytorchtools.callbacks import valid_acc_callback, valid_loss_callback, last_callback
+from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
 # import warnings
 
 
@@ -12,28 +14,41 @@ def main():
     # warnings.filterwarnings('ignore')
     # Start training from scratch
     seed_everything(args.seed)
-    if not args.resume and not args.test:
-        # Parse the argements
-        json_data = arg_parser.json_data
-        model = PatchClassifier(json_data)
-        dm = MINCDataModule(args.data_root, json_data)
-        # for HPC training
-        if torch.cuda.device_count() >= 1:
-            trainer = Trainer(progress_bar_refresh_rate=20, log_every_n_steps=20, flush_logs_every_n_steps=800,
-                              max_epochs=args.epochs,
-                              gpus=args.gpus,
-                              num_nodes=args.num_nodes,
-                              accelerator='ddp',
-                              replace_sampler_ddp=False,
-                              callbacks=[valid_acc_callback(), valid_loss_callback()])
-        else:
-            # for CPU training
-            trainer = Trainer(progress_bar_refresh_rate=1, log_every_n_steps=1, flush_logs_every_n_steps=1,
-                              max_epochs=3, replace_sampler_ddp=False, accelerator='ddp_cpu', num_processes=2,
-                              callbacks=[valid_acc_callback(), valid_loss_callback()])
+    logger = TensorBoardLogger("./lightning_logs", name=args.tag)
+
+    # Parse the argements
+    json_data = arg_parser.json_data
+    model = PatchClassifier(json_data)
+    dm = MINCDataModule(args.data_root, json_data)
+
+    # resume
+    if args.resume:
+        resume_path = os.path.join(os.getcwd(), "checkpoints", args.tag, "last", "last.ckpt")
+    else:
+        resume_path = None
+
+    # for HPC training
+    if not args.debug:
+        trainer = Trainer(progress_bar_refresh_rate=20, log_every_n_steps=20, flush_logs_every_n_steps=800,
+                          max_epochs=args.epochs,
+                          gpus=args.gpus,
+                          num_nodes=args.num_nodes,
+                          accelerator='ddp',
+                          replace_sampler_ddp=False,
+                          callbacks=[valid_acc_callback(args.tag), valid_loss_callback(args.tag), last_callback(args.tag)],
+                          logger=logger, resume_from_checkpoint=resume_path)
+    else:
+        # for CPU training
+        trainer = Trainer(progress_bar_refresh_rate=1, log_every_n_steps=1, flush_logs_every_n_steps=1,
+                          max_epochs=3, replace_sampler_ddp=False, accelerator='ddp_cpu', num_processes=2,
+                          callbacks=[valid_acc_callback(args.tag), valid_loss_callback(args.tag), last_callback(args.tag)],
+                          logger=logger, resume_from_checkpoint=resume_path)
+
+    if not args.test:
         trainer.fit(model, dm)
 
-        # run test set
+    # run test set
+    if args.test:
         trainer.test()
 
 
--- pytorchtools/callbacks.py
+++ pytorchtools/callbacks.py
@@ -1,21 +1,32 @@
 from pytorch_lightning.callbacks import ModelCheckpoint
+import os
+
+folder = "checkpoints"
 
 
-def valid_loss_callback():
+def valid_loss_callback(tag):
     return ModelCheckpoint(
         monitor="valid_loss",
-        dirpath="./checkpoints/loss",
+        dirpath=os.path.join(os.getcwd(), folder, tag, "loss"),
         filename="minc-{epoch:02d}-{valid_loss:.2f}",
         save_top_k=5,
         mode="min"
     )
 
 
-def valid_acc_callback():
+def valid_acc_callback(tag):
     return ModelCheckpoint(
         monitor="valid_accuracy",
-        dirpath="./checkpoints/accuracy",
+        dirpath=os.path.join(os.getcwd(), folder, tag, "accuracy"),
         filename="minc-{epoch:02d}-{valid_accuracy:.2f}",
         save_top_k=5,
         mode="max"
+    )
+
+
+def last_callback(tag):
+    return ModelCheckpoint(
+        save_last=True,
+        dirpath=os.path.join(os.getcwd(), folder, tag, "last"),
+        filename="last"
     )
